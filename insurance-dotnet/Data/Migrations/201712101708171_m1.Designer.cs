c2189ea406536962ec223065f25fed39a2bc0bf9
abdelkadersouibgui <abdelkader.souibgui@esprit.tn>
2017-12-10 18:18:04 +0100

    correct crud 2

--- insurance-dotnet/Data/Migrations/201712101708171_m1.Designer.cs
+++ insurance-dotnet/Data/Migrations/201712101708171_m1.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class v0 : IMigrationMetadata
+    public sealed partial class m1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(v0));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(m1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201712101637243_v0"; }
+            get { return "201712101708171_m1"; }
         }
         
         string IMigrationMetadata.Source
