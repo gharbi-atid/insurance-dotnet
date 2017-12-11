de0e1e9c420068570b7d3d257ca3034fc94f900d
abdelkadersouibgui <abdelkader.souibgui@esprit.tn>
2017-12-11 19:16:43 +0100

    consommation user

---
+++ insurance-dotnet/GUI/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Data.Models;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cgaFinaldotNet.Controllers
+{
+    public class UserController : Controller
+    {
+        UserService rs = new UserService();
+        // GET: User
+        public ActionResult Index()
+        {
+            IEnumerable<user> liste = rs.afficher();
+            return View(liste);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ insurance-dotnet/Service/UserService.cs
@@ -0,0 +1,50 @@
+﻿
+using Data.Infrastructure;
+using Service.Pattern;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Data.Models;
+
+namespace Service
+{
+    public   class UserService :Service<user> , IUserService
+    {
+        private static DatabaseFactory Dbf = new DatabaseFactory();
+    private static UnitOfWork utw = new UnitOfWork(Dbf);
+    public UserService() : base(utw)
+        {
+        }
+        public IEnumerable<user> afficher()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create
+            ("http://localhost:18080/insurance-javaee-web/insurance/reclamation");
+            request.Method = "GET";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string content = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            var objs = JsonConvert.DeserializeObject<List<user>>(content);
+            List<user> liste = new List<user>();
+            foreach (user r in objs)
+            {
+                user rec = new user();
+
+                rec = r;
+                liste.Add(rec);
+            }
+            return liste;
+        }
+
+
+
+
+    }
+}
